12e025f104135f27f1fc02043a9270e6ae6912c3
Benjamin Dumont <benjamin.dumont.ineat@axa.fr>
2018-09-03 14:39:48 +0200

    install

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-token="88f4def63f2f7f5fdb764732b68c160d139fcadc"
+#token="88f4def63f2f7f5fdb764732b68c160d139fcadc"
 
-mkdir target 2>/dev/null
-cd target
+ls -a | grep "^\.git$" 2>/dev/null
+if [ $? -ne 0 ];then
+  echo "This script only works on the root path of a git repository"
+  exit 1
+fi
+
+target="target-git-hooks"
+
+rm -rf ${target}
+mkdir ${target}  2>/dev/null
+cd ${target}
 
 wget https://raw.github.com/benjdum59/git-hooks/master/commit-msg
 
